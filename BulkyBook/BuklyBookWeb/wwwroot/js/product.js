23935b63d8b39c542433578d6f989f739d3b8036
Faisal Rasool Reshi <30429030+reshiefaisal69@users.noreply.github.com>
2022-12-29 10:08:18 +0530

    bugs rectified of datatables

--- BulkyBook/BuklyBookWeb/wwwroot/js/product.js
+++ BulkyBook/BuklyBookWeb/wwwroot/js/product.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     loadDataTable();
 });
 
-function LoadDataTable() {
+function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
             "url": "/Admin/Product/GetAll"
@@ -12,8 +12,8 @@ function LoadDataTable() {
             { "data": "title", "width": "15%" },
             { "data": "isbn", "width": "15%" },
             { "data": "price", "width": "15%" },
-            { "data": "author", "width": "15%" },
-            { "data": "category", "width": "15%" },
+            //{ "data": "author", "width": "15%" },
+            //{ "data": "category", "width": "15%" },
         ]
     });
     
